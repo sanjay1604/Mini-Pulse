46d237225362cec99a1e17bd376cd92167a085a2
Sruthi1230 <svsruthi04@gmail.com>
2024-07-10 23:03:04 +0530

    Update MiniPulseDB.java

--- src/main/java/com/minipulse/db/MiniPulseDB.java
+++ src/main/java/com/minipulse/db/MiniPulseDB.java
@@ -60,29 +60,140 @@ public interface MiniPulseDB {
                         return null;
                 }
 
-            void savePollAndOverwriteQuestions(Poll poll);
+            public void savePollAndOverwriteQuestions(Poll poll) 
+	{
+		String delquery="delete from questions where poll_id=?";
+		String insPollQuery="replace into polls(poll_id, user, title, state) VALUES(?,?,?,?)";
+		String insQuestionQuery="insert into questions (poll_id, question_id,  question_text) VALUES(?,?,?)";
+        try
+        {
+            Connection con = getConnection();
+            con.setAutoCommit(false);
+            try
             {
+                PreparedStatement delst=con.prepareStatement(delquery);
+                PreparedStatement insPollst=con.prepareStatement(insPollQuery);
+                PreparedStatement insQuesst=con.prepareStatement(insQuestionQuery);
                 
-            }
+                delst.setString(1,poll.getId());
+                delst.executeUpdate();
+                
+                insPollst.setString(1,poll.getId());
+                insPollst.setString(2,poll.getUser());
+                insPollst.setString(3,poll.getTitle());
+                insPollst.setString(4,poll.getState().name());
+                insPollst.executeUpdate();
+                
+                 for (Poll.Question question : poll.getQuestions()) {
+                                    insertQuestionst.setString(1, poll.getId());
+                                    insertQuestionst.setString(2, question.getId());
+                                    insertQuestionst.setString(3, question.getText());
+                                    insertQuestionst.addBatch();
+                                }
+                                insertQuestionst.executeBatch();
+                
+                                conn.commit();
+                    } catch (SQLException e) {
+                        conn.rollback();
+                        e.printStackTrace();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+    }
 
-             void modifyPollState(String pollId, PollState state)
-            {
-                
+public void modifyPollState(String pollId, PollState state)
+{
+    String query="update polls set state=? where poll_id=?";
+    try
+    {
+        Connection con =getConnection();
+        PreparedStatement st=con.prepareStatement(query);
+        st.setString(1,state.name());
+        st.setString(2,pollId);
+        st.executeUpdate()
+    }catch (SQLException e) {
+                e.printStackTrace();
             }
+}
 
-            Poll getPoll(String pollId)
-            {
-                
+
+ public Poll getPoll(String pollId) 
+{
+    String query="select*from polls where poll_id=?";
+    try
+    {
+        Connection con =getConnection();
+        PreparedStatement st=con.prepareStatement(query);
+        st.setString(1,pollId);
+        ResultSet rs= st.executeQuery();
+        if(rs.next())
+        {
+            String pollId_db=rs.getString("poll_id");
+            String user=rs.getString("user");
+            String title=rs.getString("title");
+            String state=rs.getString("state");
+            
+            Poll poll = new Poll(pollId_db, user, title, state);
+            return poll;
+        }
+    } catch (SQLException e) {
+                e.printStackTrace();
             }
+            return null;
+}
 
-            void deletePollEntirely(String pollId)
-            {
-                
+public void deletePollEntirely(String pollId)
+{
+    String delQuestionsQuery="delete from questions where poll_id=?";
+    String delPollQuery="delete from polls where poll_id=?";
+    try
+    {
+        Connection con = getConnection();
+        PreparedStatement delQuesst=con.prepareStatement(delQuestionsQuery);
+        PreparedStatement delPollst=con.prepareStatement(delPollQuery);
+        
+        con.setAutoCommit(false);
+        
+        delQuesst.setString(1,pollId);
+        delQuesst.executeUpdate();
+        delPollst.setString(1,pollId);
+        delPollst.executeUpdate();
+        
+        con.commit();
+    } catch (SQLException e) {
+                e.printStackTrace();
             }
-
-            Poll getPollEntirely(String pollId)
+}
+           Poll getPollEntirely(String pollId)
             {
-                
+                           String PollQuery="select* from polls where poll_id=?";
+		String QuestionsQuery="select* from questions where poll_id=?";
+		try
+		{
+			Connection con = getConnection();
+			PreparedStatement pollst=con.prepareStatement(PollQuery);
+			PreparedStatement quesst=con.prepareStatement(QuestionsQuery);	
+			pollst.setString(1,pollId);
+			ResultSet rs =pollst.executeQuery();
+			if(rs.next())
+			{
+				Poll poll = new Poll( rs.getString("poll_id"), rs.getString("user"), rs.getString("title"), PollState.valueOf(rs.getString("state")));
+				
+			quest.setString(1,pollId);
+			ResultSet qs= quest.executeQuery();
+			if(qs.next())
+			{
+				Question question = new Question( qs.getString("question_id"), qs.getString("poll
+_id"), qs.getString("question_text");
+				poll.addQuestion(question);
+			}
+		}
+			return poll;
+		} 	catch (SQLException e) {
+           	 e.printStackTrace();
+      		  }
+        	return null;
             }
 
             void newUser(String user, String userName)
