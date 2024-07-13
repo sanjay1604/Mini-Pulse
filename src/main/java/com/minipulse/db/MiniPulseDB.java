475bed557ab5fbdd056f3a64895b4e1fe99fc30c
Sruthi1230 <svsruthi04@gmail.com>
2024-07-13 15:08:42 +0530

    Update MiniPulseDB.java

--- src/main/java/com/minipulse/db/MiniPulseDB.java
+++ src/main/java/com/minipulse/db/MiniPulseDB.java
@@ -6,15 +6,6 @@ import com.minipulse.model.response.Response;
 
 import java.sql.*;
 
-public class MiniPulseDBImpl implements MiniPulseDB {
-    private static final String URL = "jdbc:mysql://localhost:3306/your_database";
-    private static final String USER = "username";
-    private static final String PASSWORD = "password";
-
-    private Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
-
 
 public interface MiniPulseDB {
 
@@ -37,69 +28,5 @@ public interface MiniPulseDB {
     void saveResponse(Response response);
 
 }
-    public class MiniPulse implements MiniPulseDB
-        {
-            String getPollIdForUserByTitle(String user, String pollTitle)
-                {
-                    String query="select  poll_id from polls where user=? and title=?";
-                        try
-                            {
-                                Connection con = getConnection();
-                                PreparedStatement st=con.prepareStatement(query);
-                                st.setString(1,user);
-                                st.setString(2,pollTitle);
-                                ResultSet rs=st.executeQuery();
-                                if(rs.next())
-                                    {
-                                        return rs.getString("poll id");
-                                    }
-                            } catch (SQLException e)
-                                {
-                                e.printStackTrace();
-                            }
-                        return null;
-                }
-
-            void savePollAndOverwriteQuestions(Poll poll);
-            {
-                
-            }
-
-             void modifyPollState(String pollId, PollState state)
-            {
-                
-            }
-
-            Poll getPoll(String pollId)
-            {
-                
-            }
-
-            void deletePollEntirely(String pollId)
-            {
-                
-            }
-
-            Poll getPollEntirely(String pollId)
-            {
-                
-            }
-
-            void newUser(String user, String userName)
-            {
-                
-            }
-            String getUser(String user)
-            {
-                
-            }
-
-            void saveResponse(Response response)
-            {
-                
-            }
-
-
-            
             
         }
